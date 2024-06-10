16e8eb4b71e93e66d7210c1e493adf89f2a218da
ito-ryo-50a <1210riytoo@gmail.com>
2024-06-11 00:02:52 +0900

    edit

---
+++ records/A - Welcome to AtCoder.rb
@@ -0,0 +1,14 @@
+# https://atcoder.jp/contests/practice/tasks/practice_1
+
+#　リファクタリングしたコード
+a = gets.chomp.to_i
+b, c = gets.split.map(&:to_i)
+s = gets.chomp
+puts "#{a + b + c} #{s}"
+
+# ACしたコード
+# map{ |value| value.to_i } と　　map(&:to_i)　は同じ内容
+number_a = gets.chomp.to_i
+number_b, number_c = gets.split.map(&:to_i)
+string_a = gets.chomp
+puts "#{number_a + number_b + number_c} #{string_a}"

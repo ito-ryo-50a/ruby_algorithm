d6b59fd98d995888c26a45746483700791216581
ito-ryo-50a <1210riytoo@gmail.com>
2024-06-16 00:04:55 +0900

    edit

---
+++ prepare_items/A - Welcome to AtCoder Land.rb
@@ -0,0 +1,13 @@
+#　https://atcoder.jp/contests/abc358/tasks/abc358_a
+
+
+#　リファクタリングしたコード
+
+
+# ACしたコード
+S, T = gets.split.map(&:to_i)
+if S == "AtCoder" && T == "Land"
+  puts "Yes"
+else
+  puts "No"
+end
